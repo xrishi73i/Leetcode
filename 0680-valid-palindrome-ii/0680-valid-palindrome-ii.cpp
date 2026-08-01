e00403087650d7be53d44d86120c75791e75015f
Rishi <xrishi73i@gmail.com>
2026-08-01 14:32:04 +0530

    Time: 0 ms (100%), Space: 22.1 MB (77.71%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool check(int l,int r,string &s){
+        while(l<r){
+            if(s[l] != s[r]){
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+    bool validPalindrome(string s) {
+        int l=0;
+        int r= s.length()-1;
+        while(l<r){
+            if(s[l] == s[r]){
+                l++;
+                r--;
+            }else{
+                return check(l+1,r,s) || check(l,r-1,s);
+            }
+        }
+        return true;
+    }
+};
